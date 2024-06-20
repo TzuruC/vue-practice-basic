9daa1ea43baf4a2bf1f48ca4a719d3bd054760a9
TzuruC <tzuruc@gmail.com>
2024-06-21 03:28:30 +0800

    if練習

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
   {{ message }}
+  <p v-show="isShow">Show World</p>
+  <p v-if="x === 0">x = 0</p>
+  <p v-else-if="x === 1">x = 1</p>
+  <p v-else-if="x === 2">x = 2</p>
+  <p v-else>x != 0</p>
 </template>
 
 <script>
@@ -8,6 +13,8 @@ export default {
   data() {
     return {
       message: "Hellow World",
+      isShow: false,
+      x: 2,
     };
   },
 };
