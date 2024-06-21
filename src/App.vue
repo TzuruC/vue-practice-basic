9d6055f0b51aae89936ee68286310d7f3080df9c
TzuruC <tzuruc@gmail.com>
2024-06-21 23:58:34 +0800

    basic for loop

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,15 @@
   <p v-else-if="x === 1">x = 1</p>
   <p v-else-if="x === 2">x = 2</p>
   <p v-else>x != 0</p>
+  <ol>
+    <li v-for="user in user1" :key="user">{{ user }}</li>
+  </ol>
+  <p v-for="(user, index) in users2" :key="user.email">
+    {{ index }} {{ user.name }} {{ user.email }}
+  </p>
+  <div v-for="(user, index) in user1" :key="user">
+    <p v-if="user === 'Jake'">{{ index }} {{ user }}</p>
+  </div>
 </template>
 
 <script>
@@ -12,9 +21,15 @@ export default {
   name: "App",
   data() {
     return {
-      message: "Hellow World",
-      isShow: false,
-      x: 2,
+      message: "Hellow World", // {{}}
+      isShow: false, // v-show, v-if
+      x: 2, // v-if
+      user1: ["Jake", "Allan", "Eason"],
+      users2: [
+        { name: "Jake", email: "jake@gmail.com" },
+        { name: "Allan", email: "allan@gmail.com" },
+        { name: "Eason", email: "eason@gmail.com" },
+      ],
     };
   },
 };
